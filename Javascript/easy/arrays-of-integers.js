5adcdd5fd00bdfe8e6d31598885d08ffc601603d
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-04-29 18:37:50 -0500

    added a solution

--- Javascript/easy/arrays-of-integers.js
+++ Javascript/easy/arrays-of-integers.js
@@ -1,2 +1,10 @@
 // Problem: You are given two arrays of integers as arguments.
 // Return true if they contain the exact same elements in any order.
+const solve = (arrOne, arrTwo) => {
+    for(let number of arrOne){
+      if(!arrTwo.includes(number)){
+        return false;
+      }
+    }
+    return true;
+};
