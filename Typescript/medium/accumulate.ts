79ad26db409845469a48a002e33c7e0c35478125
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-07-31 18:07:24 -0500

    added a new problem

---
+++ Typescript/medium/accumulate.ts
@@ -0,0 +1,26 @@
+// # Accumulate
+
+// Implement the `accumulate` operation, which, given a collection and an
+// operation to perform on each element of the collection, returns a new
+// collection containing the result of applying that operation to each element of
+// the input collection.
+
+// Given the collection of numbers:
+
+// - 1, 2, 3, 4, 5
+
+// And the operation:
+
+// - square a number (`x => x * x`)
+
+// Your code should be able to produce the collection of squares:
+
+// - 1, 4, 9, 16, 25
+
+// Check out the test suite to see the expected function signature.
+
+const accumulate = function (collection: any[], fn: (a: any) => any): any[] {
+    return collection.map(item => fn(item));
+};
+
+export default accumulate;
