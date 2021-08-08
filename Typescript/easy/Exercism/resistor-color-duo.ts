350894587bff23d2f255039b115de0a5113d8c27
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-08-08 12:29:13 -0500

    added a new problem

---
+++ Typescript/easy/Exercism/resistor-color-duo.ts
@@ -0,0 +1,55 @@
+// # Resistor Color Duo
+
+// If you want to build something using a Raspberry Pi, you'll probably use _resistors_.
+// For this exercise, you need to know two things about them:
+
+// * Each resistor has a resistance value.
+// * Resistors are small - so small in fact that if you printed the resistance value on them, it would be hard to read.
+
+// To get around this problem, manufacturers print color-coded bands onto the resistors to denote their resistance values.
+// Each band has a position and a numeric value.
+
+// The first 2 bands of a resistor have a simple encoding scheme: each color maps to a single number.
+// For example, if they printed a brown band (value 1) followed by a green band (value 5), it would translate to the number 15.
+
+// In this exercise you are going to create a helpful program so that you don't have to remember the values of the bands.
+// The program will take color names as input and output a two digit number, even if the input is more than two colors!
+
+// The band colors are encoded as follows:
+
+// - Black: 0
+// - Brown: 1
+// - Red: 2
+// - Orange: 3
+// - Yellow: 4
+// - Green: 5
+// - Blue: 6
+// - Violet: 7
+// - Grey: 8
+// - White: 9
+
+// From the example above:
+// brown-green should return 15
+// brown-green-violet should return 15 too, ignoring the third color.
+
+export class ResistorColor {
+    private colors: string[]
+  
+    constructor(colors: string[]) {
+      if(colors.length < 2) { throw new Error('At least two colors need to be present'); }
+      this.colors = colors
+    }
+    value = ():number => {
+      let resistanceValue = 0;
+      resistanceValue += colorCode(this.colors[0]) * 10; 
+      resistanceValue += colorCode(this.colors[1]);
+    
+      return resistanceValue;
+    
+    }
+  }
+  
+  const colorCode = (color: string):number => {
+    const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+    return COLORS.indexOf(color);
+};
