1b3291499b428ec93e4685a29720cfd93e806f3b
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-12-28 19:11:35 -0600

    added Strings file

---
+++ Java/Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+    // In Java world... a string is just an object that represents text. 
+    // More specifically an immutable text... but what is text from the 
+    // computer point of view?... text is just a sequence of Unicode characters
+    // (letters, digits, puntuation, etc.).
+    // strings in Java can be defined by using double quotes. 
+
+    String fruit = "Apple";
+
+    // Strings are inmmutable which means that once they are defined: They cannot change. 
+    // But Java has a big library that allows you to "manipulate" strings. However; Any method
+    // that "seems" to manipulate a string is actually returning a new string. See the String class: 
+    // https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/String.html
+}
