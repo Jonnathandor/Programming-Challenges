2607f5a8df401e36098162fb3888effadafca453
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-12-28 20:13:28 -0600

    added the first part of loglevels

---
+++ Java/easy/Exercism/LogLevels.java
@@ -0,0 +1,16 @@
+package easy.Exercism;
+
+public class LogLevels {
+    public static String message(String logLine) {
+        String regex = "\\B\\[ERROR\\]:|\\[WARNING\\]:|\\[INFO\\]:\\B";
+        return logLine.replaceAll(regex, "").trim();
+    }
+
+    public static String logLevel(String logLine) {
+        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+    }
+
+    public static String reformat(String logLine) {
+        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+    }
+}
