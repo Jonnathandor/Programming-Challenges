7cc2bc9585d4dba14833d9410ac99ff4919d8e27
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-01-01 22:01:27 -0600

    Added Java's boolean file

---
+++ Java/Booleans.java
@@ -0,0 +1,17 @@
+// Java also has booleans and are represented by the boolean type. 
+// As in other programming languages booleans only have two values: 
+// true or false. 
+// Booleans in Java suppport the following operators: 
+// !: "Not" which negates the boolean 
+// &&: "And" is found in the middle of two booleans and returns true 
+// if both booleans are true. 
+// ||: "OR" is found in the middle of two booleans and results in true
+// if at least one of the booleans is true.
+public class Booleans {
+    !true // false 
+    !false // true
+    true && false // false 
+    true && true // true
+    false || false // false 
+    false || true // true
+}
