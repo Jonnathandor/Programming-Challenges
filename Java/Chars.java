d26b9e4a7be637e9db642299e3592189fb0317b8
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-01-14 19:07:14 -0600

    Added Char basics java

---
+++ Java/Chars.java
@@ -0,0 +1,9 @@
+public class Chars {
+    // The smallest component of text is known as: char.
+    // Usually you see chars together in a form of a string.
+    // As we know a string is a way to represent words in Java.
+
+    // If you want to see the native methods available for strings visit: https://docs.oracle.com/javase/7/docs/api/java/lang/Character.html
+
+    // char literals are represented by: 'A' single quotes 
+}
