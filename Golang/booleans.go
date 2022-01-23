0da9a5a993247f6262a2c26b4bf64b8149aa9945
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-01-23 12:05:01 -0600

    added booleans in go

---
+++ Golang/booleans.go
@@ -0,0 +1,16 @@
+// booleans are a computer science concept and go... use them as what they are.
+// the bool type can only have two values: true or false and the can be combined
+// with the following boolean operators: ! (NOT), && (AND), and || (OR).
+
+true || false // Output: true
+true && false // Output: false
+!true         // Output: false
+
+// The boolean operators have a different operator precedence.
+
+// ! is evaluated first
+// && is evaluated secondly
+// || is the last one evaluated.
+
+// if those rules need to be 'ignored' you can always use () like in math
+// to isolate your expressions. 
