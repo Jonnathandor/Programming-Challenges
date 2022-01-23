de507b0883e0619743e4ce58e646e1edfd29eb2a
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-01-23 12:42:05 -0600

    added the string package

---
+++ Golang/string_package.go
@@ -0,0 +1,2 @@
+// Do not re invent the wheel
+// if you need to work with strings check the official docs: https://pkg.go.dev/strings
---
+++ Golang/strings.go
@@ -0,0 +1,11 @@
+// String like in java are an inmmutable sequence of bytes that are not always a character.
+
+// In the case fo String literals all we have is just a sequence of bytes inside double quotes
+
+name := "Jonnathan"
+
+// Like in other programming languages you can concatenate strings with the + operator.
+fullName := "Jonnathan" + " " + "Garcia"
+
+// Be aware of scaping special characters such as \t
+
