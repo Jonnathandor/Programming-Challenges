7e835e32d2efb3c9efee445f36fdc6218454d4b0
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-01-25 19:28:01 -0600

    Refactored welcome to tech palace

--- Golang/easy/Exercism/welcome_to_tech_palace.go
+++ Golang/easy/Exercism/welcome_to_tech_palace.go
@@ -2,22 +2,14 @@ package techpalace
 
 import "strings"
 
-const greetingMessage string = "Welcome to the Tech Palace, "
-
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	return greetingMessage + strings.ToUpper(customer)
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	var sb strings.Builder
-
-	for i := 0; i < numStarsPerLine; i++ {
-		sb.WriteString("*")
-	}
-
-	return sb.String() + "\n" + welcomeMsg + "\n" + sb.String()
+	return strings.Repeat("*", numStarsPerLine) + "\n" + welcomeMsg + "\n" + strings.Repeat("*", numStarsPerLine)
 }
 
 // CleanupMessage cleans up an old marketing message.
