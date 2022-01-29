d385986d59a0e2a6247035aa51c5737db816b2c9
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-01-29 08:28:40 -0600

    added booleans concepts js

---
+++ Javascript/Booleans.js
@@ -0,0 +1,5 @@
+// We use true and false to represents two truth logic values 
+// JS also contains three logical operators 
+// &&(conjuction), ||(disjunction), !(negation)
+
+// () parenthesis has the highest precedence of all JavaScript Operators. 
