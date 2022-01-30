bbe0643376c1c4d24a7563127675b45504508ad0
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-01-30 10:18:49 -0600

    added a Java term

---
+++ Java/For-Each.java
@@ -0,0 +1,28 @@
+public class ForEach {
+    // The for-each loop provides a mechanism for executing a block of
+    // code for each element in a collection. 
+
+    // for(declaration: collection) {
+    //     body;
+    // }
+
+    // The declaration declares the variable used to hold the values
+    // assigned from the collection.
+
+    // The collection is an array or collection holding
+    // the values that will be assigned to the loop variable.
+
+    // The body contains the statements that will be executed
+    // once for each value in the collection.
+
+    // For example:
+
+    // char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
+    // for(char vowel: vowels) {
+    //     System.out.println(vowel);
+    // }
+
+    // A for-each loop is guaranteed to iterate over all values.
+    
+}
