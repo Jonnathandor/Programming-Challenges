2ae46737427f23351a845531f58b39e47e76a182
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-01-29 22:40:22 -0600

    added information about chars in java

--- Java/Chars.java
+++ Java/Chars.java
@@ -6,4 +6,10 @@ public class Chars {
     // If you want to see the native methods available for strings visit: https://docs.oracle.com/javase/7/docs/api/java/lang/Character.html
 
     // char literals are represented by: 'A' single quotes 
+
+    // chars are sometimes used in conjunction with a StringBuilder object. 
+    // This object has methods that allow a string to
+    // be constructed character by character and manipulated.
+    // At the end of the process toString() can be called on it to output a
+    // complete string.
 }
