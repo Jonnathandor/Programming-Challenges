05717271c85ce63eb23077bd56bd9e2dfd888fcf
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-02-12 13:45:30 -0600

    Added a new concept for go

---
+++ Golang/float_point_numbers.go
@@ -0,0 +1,6 @@
+// Is nothing but a number with decimal values
+
+float32: 32 bits (~6-9 digits precision).
+float64: 64 bits (~15-17 digits precision). This is the default floating-point type.
+
+//By default, Go will use float64 for floating-point numbers
