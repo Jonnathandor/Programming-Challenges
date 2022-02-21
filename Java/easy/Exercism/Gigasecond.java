db6c173aeccff8653755647a5882b6655e5feb6f
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-02-21 15:50:11 -0600

    Added a new problem for Java

---
+++ Java/easy/Exercism/Gigasecond.java
@@ -0,0 +1,20 @@
+package easy.Exercism;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Gigasecond {
+    private final long GIGASECOND = 1_000_000_000L;
+    private LocalDateTime dateTime;
+    public Gigasecond(LocalDate moment) {
+        dateTime = moment.atStartOfDay();
+    }
+
+    public Gigasecond(LocalDateTime moment) {
+        dateTime = moment;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime.plusSeconds(GIGASECOND);
+    }
+}
