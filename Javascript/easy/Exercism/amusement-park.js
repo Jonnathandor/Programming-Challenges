90c93d0f842a4f099c5898e2dc8121e3a99dd022
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-04-06 05:26:39 -0500

    added createVisitor and revokeTicket

--- Javascript/easy/Exercism/amusement-park.js
+++ Javascript/easy/Exercism/amusement-park.js
@@ -145,3 +145,29 @@
 // ### Created by
 
 // - @junedev
+
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new visitor.
+ *
+ * @param {string} name
+ * @param {number} age
+ * @param {string} ticketId
+ * @returns {Visitor} the visitor that was created
+ */
+ export function createVisitor(name, age, ticketId) {
+    return { name, age, ticketId };
+  }
+  
+  /**
+   * Revokes a ticket for a visitor.
+   *
+   * @param {Visitor} visitor the visitor with an active ticket
+   * @returns {Visitor} the visitor without a ticket
+   */
+  export function revokeTicket(visitor) {
+    visitor.ticketId = null;
+    return visitor;
+  }
