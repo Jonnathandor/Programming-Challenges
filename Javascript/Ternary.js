956b439e16165ba8fea6ad7103d4e2d98679cf36
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-04-11 14:13:41 -0500

    Added Ternary concept js

---
+++ Javascript/Ternary.js
@@ -0,0 +1,8 @@
+// The name stems from the fact that the operator has three operands:
+// predicate ? consequent-expression : alternative-expression
+// It can be used as a replacement for short if-else statements.
+
+const year = 2020;
+
+year > 2000 ? 'in the past years' : 'a long time ago';
+// => 'in the past years'
