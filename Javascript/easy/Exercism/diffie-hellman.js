145726497ba90be1f2d48485a14e2a4cd1dfe39d
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-04-21 21:08:28 -0500

    added a new method

--- Javascript/easy/Exercism/diffie-hellman.js
+++ Javascript/easy/Exercism/diffie-hellman.js
@@ -46,9 +46,14 @@
 export class DiffieHellman {
     #privateKeyA;
     #privateKeyB;
-    
+
     constructor(p, g) {
       if(p < 1 || p > 9999 || g < 1 || g > 9999) throw Error('Out of range');
       if(p % 2 === 0 || g % 2 === 0) throw Error('The arguments are not prime numbers');
+    }
+
+    getPublicKey(privateKey) {
+        if(privateKey <= 1 || privateKey >= this.#privateKeyA) throw Error('invalid key');
+        return (this.#privateKeyB ** privateKey) % this.#privateKeyA;
     }
 }
