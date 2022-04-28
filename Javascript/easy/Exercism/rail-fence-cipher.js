4edd3458fa5a24e6c2e41dd1124eb3263cfca2e6
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-04-27 19:34:01 -0500

    refactored encode method

--- Javascript/easy/Exercism/rail-fence-cipher.js
+++ Javascript/easy/Exercism/rail-fence-cipher.js
@@ -1,5 +1,10 @@
 export const encode = (text, depth) => {
-  
+  const letters = text.split('');
+  if(depth === 2){
+    // problem ... do I really wnat to use two filters?? 
+    // this solution is only good with a dept of two
+    return letters.filter((letter, i) => i % 2 === 0).join('') + letters.filter((letter, i) => i % 2 !== 0).join('');
+  }
 };
   
 export const decode = () => {
