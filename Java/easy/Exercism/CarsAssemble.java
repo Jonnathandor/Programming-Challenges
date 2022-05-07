414972dc6e8e233e562e892ea16404fcab885ca2
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-05-07 14:57:33 -0500

    refactored CarsAssemble

--- Java/easy/Exercism/CarsAssemble.java
+++ Java/easy/Exercism/CarsAssemble.java
@@ -41,26 +41,30 @@ package easy.Exercism;
 // - @TalesDias
 
 public class CarsAssemble {
-    final int carsPerHour = 221;
+    final static int PRODUCTION_BASE_RATE = 221;
 
     public double productionRatePerHour(int speed) {
-        if(speed == 0) { return 0; }
-        if(speed >= 10){
-            return this.productionCalculator(speed, carsPerHour, .77);
-        } else if(speed == 9){
-            return this.productionCalculator(speed, carsPerHour, .8);
-        } else if(speed >= 5){
-            return this.productionCalculator(speed, carsPerHour, .9);
+        if(speed == 0) {
+            return 0;
+        }
+        else if(speed < 5){
+            return speed * PRODUCTION_BASE_RATE;
+        }
+        else if(speed < 9){
+            return this.productionCalculator(speed, PRODUCTION_BASE_RATE, .9);
+        }
+        else if(speed < 10){
+            return this.productionCalculator(speed, PRODUCTION_BASE_RATE, .8);
         }
 
-        return speed * carsPerHour;
+        return this.productionCalculator(speed, PRODUCTION_BASE_RATE, .77);
     }
 
     public int workingItemsPerMinute(int speed) {
         return (int)this.productionRatePerHour(speed) / 60;
     }
 
-    private double productionCalculator(int speed, int carsPerHour, double percentage) {
-        return speed * carsPerHour * percentage;
+    private double productionCalculator(int speed, int baseRate, double percentage) {
+        return speed * baseRate * percentage;
     }
 }
