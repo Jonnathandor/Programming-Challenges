e7c262168b03407e09ccc9f5280f416ac1e0cefb
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-05-23 14:36:37 -0500

    cipher WIP

--- Javascript/easy/Exercism/rail-fence-cipher.js
+++ Javascript/easy/Exercism/rail-fence-cipher.js
@@ -1,4 +1,73 @@
+// ## Instructions
+
+// Implement encoding and decoding for the rail fence cipher.
+
+// The Rail Fence cipher is a form of transposition cipher that gets its name from
+// the way in which it's encoded. It was already used by the ancient Greeks.
+
+// In the Rail Fence cipher, the message is written downwards on successive "rails"
+// of an imaginary fence, then moving up when we get to the bottom (like a zig-zag).
+// Finally the message is then read off in rows.
+
+// For example, using three "rails" and the message "WE ARE DISCOVERED FLEE AT ONCE",
+// the cipherer writes out:
+
+// ```text
+// W . . . E . . . C . . . R . . . L . . . T . . . E
+// . E . R . D . S . O . E . E . F . E . A . O . C .
+// . . A . . . I . . . V . . . D . . . E . . . N . .
+// ```
+
+// Then reads off:
+
+// ```text
+// WECRLTEERDSOEEFEAOCAIVDEN
+// ```
+
+// To decrypt a message you take the zig-zag shape and fill the ciphertext along the rows.
+
+// ```text
+// ? . . . ? . . . ? . . . ? . . . ? . . . ? . . . ?
+// . ? . ? . ? . ? . ? . ? . ? . ? . ? . ? . ? . ? .
+// . . ? . . . ? . . . ? . . . ? . . . ? . . . ? . .
+// ```
+
+// The first row has seven spots that can be filled with "WECRLTE".
+
+// ```text
+// W . . . E . . . C . . . R . . . L . . . T . . . E
+// . ? . ? . ? . ? . ? . ? . ? . ? . ? . ? . ? . ? .
+// . . ? . . . ? . . . ? . . . ? . . . ? . . . ? . .
+// ```
+
+// Now the 2nd row takes "ERDSOEEFEAOC".
+
+// ```text
+// W . . . E . . . C . . . R . . . L . . . T . . . E
+// . E . R . D . S . O . E . E . F . E . A . O . C .
+// . . ? . . . ? . . . ? . . . ? . . . ? . . . ? . .
+// ```
+
+// Leaving "AIVDEN" for the last row.
+
+// ```text
+// W . . . E . . . C . . . R . . . L . . . T . . . E
+// . E . R . D . S . O . E . E . F . E . A . O . C .
+// . . A . . . I . . . V . . . D . . . E . . . N . .
+// ```
+
+// If you now read along the zig-zag shape you can read the original message.
+
 const encode = (text, depth) => {
+  // hashmap technique. Something like, based on depth populate the hashmap initially.
+
+  // message = 'WEAREDISCOVEREDFLEEATONCE'
+  // depth = '3'
+  // let hashMap = {1:'',2:'',3:''}
+  // we can then loop the message and populate the hashmap, 
+  // i.e keep a counter variable(key)(initial value 1) and based on the 
+  // direction either zig or zag increment/decrement the var. eg: hashMap[counter] = message[i]
+
   let encoded = '';
   let letters = buildLetterMap({}, depth)
   letters = populateLetterMap(text.length -1, 1, false, depth, letters, text);
@@ -85,6 +154,8 @@ const createDecodedMessage = (columns, key, goesUp, depth, letters) => {
       goesUp = !goesUp
     }
     //console.log(letters[key][i]);
+    // I have no idea how to build the 
+    // decoded message
     decoded += letters[key][i - 1? 0 : i - 1];
 
     console.log(letters[key]);
