001a236881ccdb38905854894cc1b2e801601650
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-05-28 17:29:57 -0500

    modified stock profits

--- SWE/easy/MaxStocksProfits.js
+++ SWE/easy/MaxStocksProfits.js
@@ -3,6 +3,13 @@
 
 // index: 0, 1, 2, 3, 4, 5
 // prices: [8,1,4,2,8,9]
+// day 1 = 1
+// day 5 = 9
+// in order to have the best profit we would buy on day 1 and sell in day 5
+
+// We need to figure out what is the cheapest day and the highest one to calculate the best profit
+// so we can use a loop to iterate the prices and keep track of the lowest price 
+// and the difference between the the current stock price and the lowest price seen.
 const maxPorfit = (prices) => {
     let minPrice = prices[0];
     let maxDiff = 0;
