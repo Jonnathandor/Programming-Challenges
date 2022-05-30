488cf000397a4b0a673ef12ef7c3bbb6c3a08461
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-05-29 20:22:25 -0500

    added a new problem

---
+++ SWE/easy/FindPivotIndex.js
@@ -0,0 +1,20 @@
+// Given an array of integers, return the index such that the sum of the integers 
+// on the left is equal to the sum of the integers on the right. If no index exists, 
+// return -1. If there are multiple indexes that exist, return the left-most one.
+
+// Example 1:
+
+// Input: 
+// nums = [1, 7, 3, 6, 5, 6]
+// Output: 3
+// Explanation: 
+// The sum of the integers on the left of index 3 is equal to the sum of integers on the right of index 3.
+ 
+
+// Example 2:
+
+// Input: 
+// nums = [1, 2, 3]
+// Output: -1
+// Explanation: 
+// No index exists that can satisfy the problem statement.
