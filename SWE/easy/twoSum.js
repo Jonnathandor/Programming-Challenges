4c994986b2fabfd64dade8288c3f44a4a52ee432
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-06-09 20:17:03 -0500

    added a new problem

---
+++ SWE/easy/twoSum.js
@@ -0,0 +1,11 @@
+// Given an array of numbers and a target value, return the indices
+// of two numbers that add up to the target value. 
+// You can assume there is always only one correct answer in the given array.
+
+ 
+
+// Example:
+
+// Given nums = [2, 7, 11, 15], target = 9,
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
