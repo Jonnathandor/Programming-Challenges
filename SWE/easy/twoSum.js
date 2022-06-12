e7436db3991dc534073163bc61f015aac55e9a1c
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-06-11 19:57:25 -0500

    added a new solution

--- SWE/easy/twoSum.js
+++ SWE/easy/twoSum.js
@@ -2,10 +2,27 @@
 // of two numbers that add up to the target value. 
 // You can assume there is always only one correct answer in the given array.
 
- 
-
 // Example:
-
 // Given nums = [2, 7, 11, 15], target = 9,
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
+
+const twoSum = (nums, target) => {
+    // The hash map stores the complement value
+    // to the index.
+
+    // We store the difference between the target and 
+    // every value in the hashmap because 
+    // the difference between the trget and the value
+    // will allow us to find the other pair that adds
+    // up to the target value.
+    const complements = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] in complements){
+            return [nums[i], complements[i]];
+        }
+
+        complements[i] = target - nums[i];
+    }
+};
