e928664c6d8e0332f3a0fae9ec6ff2079d425844
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-06-22 17:10:23 -0500

    fixed packages

--- Java/easy/random/MinutesToYearsDaysCalculator.java
+++ Java/easy/random/MinutesToYearsDaysCalculator.java
@@ -1,3 +1,5 @@
+package easy.random;
+
 // Write a method printYearsAndDays with parameter of type long named minutes.
 // The method should not return anything (void) and it needs to calculate the years and days from the minutes parameter.
 // If the parameter is less than 0, print text "Invalid Value".
