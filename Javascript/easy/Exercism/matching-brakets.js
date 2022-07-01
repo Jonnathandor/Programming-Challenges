93deb8cf83b3c69929af4bf768b3f9063dcfd886
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-07-01 15:33:26 -0500

    added a new problem

---
+++ Javascript/easy/Exercism/matching-brakets.js
@@ -0,0 +1,7 @@
+// ## Instructions
+
+// Given a string containing brackets `[]`, braces `{}`, parentheses `()`,
+// or any combination thereof, verify that any and all pairs are matched
+// and nested correctly.
+
+// We could potentially use the stack data structure. 
