c047b6719670b4101be6352c70883bf5b317f759
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-07-02 18:50:49 -0500

    added a new solution

--- Javascript/easy/Exercism/matching-brakets.js
+++ Javascript/easy/Exercism/matching-brakets.js
@@ -5,3 +5,29 @@
 // and nested correctly.
 
 // We could potentially use the stack data structure. 
+
+// if opening bracket we can push to the stack
+// if closing bracket we want to check the stack 
+// to see if there is an opening pair there. 
+
+const BRACKET_MAP = {
+    '{': '}',
+    '[': ']',
+    '(': ')'
+};
+
+export const isPaired = (str) => {
+    if(str.length === 0) return true;
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+
+        if(str[i] in BRACKET_MAP) {
+            stack.push(str[i]);
+        } else if (Object.values(BRACKET_MAP).includes(str[i])){
+            if(BRACKET_MAP[stack.pop()] !== str[i]) return false;
+        }
+    }
+
+    return stack.length === 0;
+};
