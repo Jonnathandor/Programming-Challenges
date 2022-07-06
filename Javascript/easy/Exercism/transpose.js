113a2cb5858b340f5e7399d57fc44e1fa77eb4dc
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-07-05 20:47:33 -0500

    added a new solution

--- Javascript/easy/Exercism/transpose.js
+++ Javascript/easy/Exercism/transpose.js
@@ -57,6 +57,18 @@
 // In general, all characters from the input should also be present in the transposed output.
 // That means that if a column in the input text contains only spaces on its bottom-most row(s),
 // the corresponding output row should contain the spaces in its right-most column(s).
-export const transpose = () => {
+export const transpose = (matrix) => {
+    if(matrix.length === 0) return matrix;
+    const longestElement = Math.max(...(matrix.map(element => element.length)));
+    const transposed = [];
   
+    for (let i = 0; i < longestElement; i++) {
+      let col = '';
+      for (let j = 0; j < matrix.length; j++) {
+        matrix[j][i] === undefined ? col += ' ' : col += matrix[j][i];
+      }
+      transposed.push(col);
+    }
+  
+    return transposed;
 };
