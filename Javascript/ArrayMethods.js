12b64acaf7c85893ce562ea66b285e362cccc7aa
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2022-07-18 18:53:45 -0500

    WIP

--- Javascript/ArrayMethods.js
+++ Javascript/ArrayMethods.js
@@ -52,3 +52,34 @@ const sortedLastNames = people.sort((a, b) => {
     return aLast > bLast ? 1 : -1;
 });
 console.table(sortedLastNames);
+
+const morePeople = [
+    { name: 'Jon', year: 1992 },
+    { name: 'Charles', year: 1985 },
+    { name: 'Andrew', year: 1995 },
+    { name: 'Lex', year: 1990 }
+];
+
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// is at least one person 19?
+// true
+const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+// is everyone 19?
+// false
+const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+const comment = comments.find(comment => comment.id === 823423);
+
+// gets the index
+const index = comments.findIndex(comment => comment.id === 823423);
+
