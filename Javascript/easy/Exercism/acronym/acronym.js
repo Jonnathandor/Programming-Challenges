420d67465fa7da140c59ce9f4691ea5c93ad3679
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2023-02-06 21:48:02 -0600

    added explanation for regular expressions

--- Javascript/easy/Exercism/acronym/acronym.js
+++ Javascript/easy/Exercism/acronym/acronym.js
@@ -6,3 +6,10 @@ export const parse = (s) => {
   }
   return acronym;
 };
+
+// [^a-zA-Z']: This is the character class, it matches any character that is not lowercase a-z, uppercase A-Z, or an apostrophe '.
+
+// +: The plus sign specifies that the preceding character class must match one or more times.
+// This means the regular expression will match one or more characters that are not letters or apostrophes.
+
+// /g: This is the global flag, it specifies that the regular expression should search for all occurrences of the pattern, not just the first one.
