c20d9f5f290a36ef48e20607ffaff4238ef7878f
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2023-02-13 19:53:55 -0600

    added a new problem

---
+++ Java/easy/Exercism/scrabble-score/src/main/java/Scrabble.java
@@ -0,0 +1,58 @@
+class Scrabble {
+    private final String word;
+
+    Scrabble(String word) {
+        this.word = word;
+    }
+
+    int getScore() {
+        int score = 0;
+        for (int i = 0; i < word.length(); i++) {
+            char letter = Character.toUpperCase(word.charAt(i));
+            switch (letter) {
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                case 'L':
+                case 'N':
+                case 'R':
+                case 'S':
+                case 'T':
+                    score += 1;
+                    break;
+                case 'D':
+                case 'G':
+                    score += 2;
+                    break;
+                case 'B':
+                case 'C':
+                case 'M':
+                case 'P':
+                    score += 3;
+                    break;
+                case 'F':
+                case 'H':
+                case 'V':
+                case 'W':
+                case 'Y':
+                    score += 4;
+                    break;
+                case 'K':
+                    score += 5;
+                    break;
+                case 'J':
+                case 'X':
+                    score += 8;
+                    break;
+                case 'Q':
+                case 'Z':
+                    score += 10;
+                    break;
+            }
+        }
+        return score;
+    }
+
+}
