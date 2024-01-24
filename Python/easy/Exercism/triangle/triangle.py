be497ecc8f99b053601521c50410390f381e793c
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2024-01-24 13:02:29 -0600

    added a solution for triangle

--- Python/easy/Exercism/triangle/triangle.py
+++ Python/easy/Exercism/triangle/triangle.py
@@ -1,10 +1,19 @@
 def equilateral(sides):
-    pass
-
+    return len(set(sides)) == 1 and all(side > 0 for side in sides)
 
 def isosceles(sides):
-    pass
-
+    return len(set(sides)) <= 2 and all(side > 0 for side in sides) and not violates_triangle_inequality(sides)
 
 def scalene(sides):
-    pass
+    return len(set(sides)) == 3 and all(side > 0 for side in sides) and not violates_triangle_inequality(sides)
+
+def violates_triangle_inequality(sides):
+    a, b, c = sorted(sides)
+    # Check the Triangle Inequality Theorem which states that the sum of the lengths
+    # of any two sides of a triangle must be greater than the length of the third side.
+    # This function returns True if the theorem is violated, indicating that the sides
+    # cannot form a valid triangle. If the sum of the shorter two sides (a and b) is
+    # less than or equal to the length of the longest side (c), the sides do not satisfy
+    # the theorem, hence the triangle inequality is violated.
+    return a + b <= c
+
