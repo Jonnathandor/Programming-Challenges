2f848c888e5e4554fc06ff04ad95c7741af2c416
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2024-01-28 16:35:45 -0600

    Finshed Bob Challenge

--- Python/easy/Exercism/bob/bob.py
+++ Python/easy/Exercism/bob/bob.py
@@ -3,3 +3,17 @@ def response(hey_bob):
 
     if not hey_bob:
         return "Fine. Be that way!"
+
+    is_question = hey_bob.endswith('?')
+    is_shouting = hey_bob.isupper()
+
+    if is_question and is_shouting: 
+        return "Calm down, I know what I'm doing!"
+    
+    if is_shouting: 
+        return "Whoa, chill out!"
+    
+    if is_question: 
+        return "Sure."
+    
+    return "Whatever."
