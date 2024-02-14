32be10c636952ee8b73d114ac108bb9abea4b582
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2024-02-14 05:21:34 -0600

    added new challenges

---
+++ Python/easy/Python Pro Bootcamp/variables.py
@@ -0,0 +1,11 @@
+# This program takes two inputs. The first input is stored in a variable called a. 
+# The second input is stored in a variable called b.
+
+# Write a program that switches the values stored in the variables a and b.
+a = input()
+b = input()
+
+a, b = b, a
+
+
+
