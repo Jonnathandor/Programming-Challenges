eb4a75bc943037089c1226fd6f92270d03c54e36
Jonnathandor <jonnathandor@gmail.com>
2026-07-09 00:56:54 -0500

    added a new challenge

---
+++ Java/easy/coddy/gallflowerGreetings.java
@@ -0,0 +1,8 @@
+// Write a program that initializes a String variable with "Greetings, Gallflower!" and prints it.
+class Main {
+    public static void main(String[] args) {
+        // Write code here
+        System.out.println("Greetings, Gallflower!");
+        // Don't change below this line
+    }
+}
